0e8a50fea15d5d0bbf03414a2451c93f9849eea4
bdjm <brendandjmoore@gmail.com>
2021-05-03 14:56:24 +1000

    adding function to add badges to licenses

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license.toUpperCase() == "MIT") {
+        return "https://img.shields.io/badge/License-MIT-red";
+    } else if (license.toUpperCase() == "APACHE 2.0") {
+        return "https://img.shields.io/badge/License-APACHE%202.0-brightgreen";
+    } else if (license.toUpperCase() == "GPL 3.0") {
+        return "https://img.shields.io/badge/License-GPL%203.0-blue";
+    } else if (license.toUpperCase() == "BSD 3") {
+        return "https://img.shields.io/badge/License-BSD%203-orange";
+    } else {
+        return "";
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
